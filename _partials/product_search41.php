902f05b0c7b2c103ddc21c51b144927e5b0e9dd3
nikitazukov1997 <bolesalavb@gmail.com>
2021-06-22 00:58:20 +0900

    fix search module

--- _partials/product_search41.php
+++ _partials/product_search41.php
@@ -1,46 +1,45 @@
 <?php
+          $root_dir = __DIR__.'/..';
           $car_manu = isset($_POST['car_manu'])?$_POST['car_manu']:'';
           $car_model_cat = isset($_POST['car_model_cat'])?$_POST['car_model_cat']:'';
           $model = isset($_POST['model'])?$_POST['model']:'';
-          $file = $root_dir.'/db/car_manus.csv';
-          $handle = fopen($file, "r");
           $car_manus = []; $car_model_cats = []; $product_models = []; $filtered_products = [];
+          $filename = basename($_SERVER['PHP_SELF'], '.php');
           
-          while (($row = fgetcsv($handle, 0, ",")) !== false) 
-          {
-              if($row[0] != '') $car_manus[$row[0]] = $row[0];
-          }
-          
-          fclose($handle);
-          
-          if($car_manu != ''){
             $file = $root_dir.'/db/item.csv';
-            // $handle = fopen($file, "r");
             $buffer=explode("\n",file_get_contents($file));
-            // echo(count($buffer));
             foreach($buffer as $row_buf){
                 $row = explode(",", $row_buf);
                 if(count($row) < 19) continue;
                 
                 $product = new stdClass();
                 $product->car_manu = $row[7];
-                
-                if($product->car_manu == $car_manu){
+                if($row[7] == "" || $row[0] != $filename) continue;
+                $product = new stdClass();
+                $product->car_manu = $row[7];
+                $car_manus[$row[7]] = $row[7];
+                if($product->car_manu == $car_manu && $car_manu != ''){
                   $product->car_model_cat = $row[1];
                   if($product->car_model_cat != ''){
                     $car_model_cats[$product->car_model_cat] = $product->car_model_cat;
                     if($car_model_cat == $product->car_model_cat && $car_model_cat != ''){
                       $product->manufacturer_name = $row[3];
                       $product->product_name = $row[4];
-                      $product->price = $row[18];
+                      $product->price = $row[17];
                       $product->car_type = $row[8];
                       $product->model = $row[9];
                       $product->model_year = $row[10];
                       $product->driving = $row[11];
                       $product->compliance_details = $row[12];
                       $product->specification = $row[13];
+                      $product->id = $row[14];
                       $product->manu_part_number = $row[5];
                       $product_models [$product->model]= $product->model;
+                      if($product->price == "出さない"){
+                        $product->price = "お問い合わせください";
+                      }else{
+                        $product->price = number_format($product->price);
+                      }
                       if($model != '' && $model == $product->model)
                         $filtered_products []= $product;
                     }	
@@ -49,8 +48,6 @@
                 }
             }
           
-            // fclose($handle);
-          }
           
           ?>
           <div class="search-block grey-wrapper" id="search-block">
@@ -65,7 +62,7 @@
             <div class="clearfix"></div>
             <div class="clearfix"></div>
             <div class="clearfix"></div>
-            <form action="/<?=$uri?>" method="post" class="row ja">
+            <form action="#search-block" method="post" class="row ja">
               <div class="search-select col-md-4 col-sm-4">
                 <select class="custom-select-lg" name="car_manu"
                   onchange="this.form.car_model_cat=''; this.form.model = ''; submit(this.form)">
@@ -86,7 +83,7 @@
                 </select>
               </div>
               <div class="search-select col-md-4 col-sm-4">
-                <select class="custom-select-lg" name="model">
+                <select class="custom-select-lg" name="model" onchange="enableSearchButton(this.value);">
                   <option value='' <?php if($model == '') echo 'selected'; ?>>型式を選ぶ</option>
                   <?php foreach ($product_models as $key => $value) { ?>
                   <option value="<?=$value?>" <?php if($model == $value) echo 'selected'; ?>><?=$value?></option>
@@ -97,15 +94,14 @@
               <div class="clearfix"></div>
               <div class="clearfix"></div>
               <div class="clearfix"></div>
-              <button type="submit" class="btn-search">検索</button>
+              <button type="submit" class="btn-search" disabled>検索</button>
             </form>
             <?php if(count($filtered_products) > 0){ ?>
             <div class="search-results">
               <table class="matching_table_all">
                 <thead>
+                  <!-- 【価格・車種・型式・年式・駆動・適合詳細・仕様・メーカー品番】 -->
                   <tr>
-                    <th>メーカー名</th>
-                    <th>商品名</th>
                     <th>価格</th>
                     <th>車種</th>
                     <th>型式</th>
@@ -118,9 +114,7 @@
                 </thead>
                 <tbody>
                   <?php foreach ($filtered_products as $key => $value) { ?>
-                  <tr>
-                    <td><?=$value->manufacturer_name?></td>
-                    <td><?=$value->product_name?></td>
+                  <tr onclick="window.location = 'https://www.kts-web.com/ec_shop/products/detail/<?=$value->id?>'">
                     <td><?=$value->price?></td>
                     <td><?=$value->car_type?></td>
                     <td><?=$value->model?></td>
@@ -138,5 +132,14 @@
             <div class="clearfix"></div>
           </div>
           <div class="item_info_text_small">
-                  ▼ご希望の商品をタップすると詳細・購入ページに遷移します。<br>※表示価格は税抜です。<br>※輸入車は平和島店・一之江店のみの対応です。</div>
+                  ▼ご希望の商品をタップすると詳細・購入ページに遷移します。<br>※表示価格は税込です。<br>※輸入車は平和島店・一之江店のみの対応です。</div>
           <div class="clearfix"></div>
+<script>
+function enableSearchButton(value){
+  if(value != ""){
+    document.getElementsByClassName("btn-search")[0].disabled=false;
+  }else{
+    document.getElementsByClassName("btn-search")[0].disabled=true;
+  }
+}
+</script>
